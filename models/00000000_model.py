2938174495f22af4c27585cd1f9ff38af447e5f5
garden1ee <little0potato@kaist.ac.kr>
2023-04-22 01:59:36 +0900

    update model to transformer

--- models/00000000_model.py
+++ models/00000000_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from typing import Dict
 
 from . import BaseModel, register_model
@@ -7,15 +8,15 @@ from . import BaseModel, register_model
 @register_model("00000000_model")
 class MyModel00000000(BaseModel):
 
-    def __init__(self, input_size=128, hidden_size=512, num_layers=1, dropout_rate=0.2, **kwargs):
+    def __init__(self, input_size=128, hidden_size=512, num_layers=2, dropout_rate=0.2, **kwargs):
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
-        # Define the lstm layer
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        
+        # Define the transformer layer
+        self.transformer = nn.TransformerEncoderLayer(d_model=input_size, nhead=8, dim_feedforward=hidden_size)
+
         # Define the batch normalization layer
         self.batchnorm = nn.BatchNorm1d(hidden_size)
         
@@ -26,34 +27,34 @@ class MyModel00000000(BaseModel):
         self.relu = nn.ReLU()
 
         self.task_layers = nn.ModuleDict({
-            "short_mortality": nn.Linear(hidden_size, 1),
-            "long_mortality": nn.Linear(hidden_size, 1),
-            "readmission": nn.Linear(hidden_size, 1),
-            "diagnosis_1": nn.Linear(hidden_size, 1), # 17 diagnoses, each binary
-            "diagnosis_2": nn.Linear(hidden_size, 1),
-            "diagnosis_3": nn.Linear(hidden_size, 1),
-            "diagnosis_4": nn.Linear(hidden_size, 1),
-            "diagnosis_5": nn.Linear(hidden_size, 1),
-            "diagnosis_6": nn.Linear(hidden_size, 1),
-            "diagnosis_7": nn.Linear(hidden_size, 1),
-            "diagnosis_8": nn.Linear(hidden_size, 1),
-            "diagnosis_9": nn.Linear(hidden_size, 1),
-            "diagnosis_10": nn.Linear(hidden_size, 1),
-            "diagnosis_11": nn.Linear(hidden_size, 1),
-            "diagnosis_12": nn.Linear(hidden_size, 1),
-            "diagnosis_13": nn.Linear(hidden_size, 1),
-            "diagnosis_14": nn.Linear(hidden_size, 1),
-            "diagnosis_15": nn.Linear(hidden_size, 1),
-            "diagnosis_16": nn.Linear(hidden_size, 1),
-            "diagnosis_17": nn.Linear(hidden_size, 1),
-            "short_los": nn.Linear(hidden_size, 1),
-            "long_los": nn.Linear(hidden_size, 1),
-            "final_acuity": nn.Linear(hidden_size, 6),
-            "imminent_discharge": nn.Linear(hidden_size, 6),
-            "creatinine_level": nn.Linear(hidden_size, 5),
-            "bilirubin_level": nn.Linear(hidden_size, 5),
-            "platelet_level": nn.Linear(hidden_size, 5),
-            "wbc_level": nn.Linear(hidden_size, 3),
+            "short_mortality": nn.Linear(input_size, 1),
+            "long_mortality": nn.Linear(input_size, 1),
+            "readmission": nn.Linear(input_size, 1),
+            "diagnosis_1": nn.Linear(input_size, 1), # 17 diagnoses, each binary
+            "diagnosis_2": nn.Linear(input_size, 1),
+            "diagnosis_3": nn.Linear(input_size, 1),
+            "diagnosis_4": nn.Linear(input_size, 1),
+            "diagnosis_5": nn.Linear(input_size, 1),
+            "diagnosis_6": nn.Linear(input_size, 1),
+            "diagnosis_7": nn.Linear(input_size, 1),
+            "diagnosis_8": nn.Linear(input_size, 1),
+            "diagnosis_9": nn.Linear(input_size, 1),
+            "diagnosis_10": nn.Linear(input_size, 1),
+            "diagnosis_11": nn.Linear(input_size, 1),
+            "diagnosis_12": nn.Linear(input_size, 1),
+            "diagnosis_13": nn.Linear(input_size, 1),
+            "diagnosis_14": nn.Linear(input_size, 1),
+            "diagnosis_15": nn.Linear(input_size, 1),
+            "diagnosis_16": nn.Linear(input_size, 1),
+            "diagnosis_17": nn.Linear(input_size, 1),
+            "short_los": nn.Linear(input_size, 1),
+            "long_los": nn.Linear(input_size, 1),
+            "final_acuity": nn.Linear(input_size, 6),
+            "imminent_discharge": nn.Linear(input_size, 6),
+            "creatinine_level": nn.Linear(input_size, 5),
+            "bilirubin_level": nn.Linear(input_size, 5),
+            "platelet_level": nn.Linear(input_size, 5),
+            "wbc_level": nn.Linear(input_size, 3),
         })
         
     def get_logits(cls, net_output):
@@ -67,22 +68,12 @@ class MyModel00000000(BaseModel):
 
         x = data.view(-1, 100, 128)
 
-        # Initialize hidden and cell state
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        # Pass the input through the transformer layer
+        x = self.transformer(x)
         
-        # Pass the input through the LSTM layer
-        x, _ = self.lstm(x, (h0, c0))
-        
-        # Apply batch normalization
-        x = self.batchnorm(x[:, -1, :])
+        # Apply mean pooling along the sequence dimension
+        x = torch.mean(x, dim=1)
 
-        # Apply ReLU activation function
-        x = self.relu(x)
-        
-        # Apply dropout
-        x = self.dropout(x)
-        
         # Pass the output through the task specific layers
         output = {}
         
